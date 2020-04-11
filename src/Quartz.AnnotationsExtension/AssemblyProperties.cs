4090e053370570ef38943057ae3c658c91c8837d
Nuno Caneco <nuno.caneco@gmail.com>
2020-04-11 08:39:43 +0100

    chore: enable internals visible to on Release
    To allow running Unit Tests with the Release build.

--- src/Quartz.AnnotationsExtension/AssemblyProperties.cs
+++ src/Quartz.AnnotationsExtension/AssemblyProperties.cs
@@ -1,8 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
 
-#if DEBUG
 [assembly: InternalsVisibleTo("Quartz.AttributesExtension.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // To allow Moq to generate Mocks from internal interfaces
-#endif
-
-[assembly: InternalsVisibleTo("Quartz.AttributesExtension.PerfTests")] // Perf tests must be executing in Release configuration
+[assembly: InternalsVisibleTo("Quartz.AttributesExtension.PerfTests")]
